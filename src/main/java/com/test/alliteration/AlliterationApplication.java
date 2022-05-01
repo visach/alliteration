2e8b4987c89ba8ab780ef283322b05a753dd0222
visach <visach@pm.me>
2022-05-01 08:57:59 +0200

    Word counter achieved

--- src/main/java/com/test/alliteration/AlliterationApplication.java
+++ src/main/java/com/test/alliteration/AlliterationApplication.java
@@ -3,6 +3,8 @@ package com.test.alliteration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class AlliterationApplication {
 
@@ -11,6 +13,23 @@ public class AlliterationApplication {
 
 		System.out.println("Hello!");
 		System.out.println("Welcome to Alliteration App");
+
+		String easyTest = "Mike made mellow music with his new microphone";
+		String hardTest = "Mike! ,made$ %mellow/music wi(th his new ,microphone.";
+
+		//Possible patterns
+		//String splited[] = source_.split("\\W");
+		//String splited[] = source_.split("[a-zA-Z]");
+		//String spl_[] = easyStr_.split(" ");
+
+		//Easy one
+		Stream<String> easyStream = Stream.of(easyTest.split(" "));
+
+		long counter = easyStream.count();
+		System.out.println("Total words: " + counter);
+
+		//easyStream.forEach(s-> System.out.println(s));
+
 	}
 
 }
