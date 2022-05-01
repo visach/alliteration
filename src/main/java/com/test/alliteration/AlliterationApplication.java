d05fbfe5dcd3fbdf159c4594d1e4e2296a41676e
visach <visach@pm.me>
2022-05-01 07:31:06 +0200

    Hello world!

---
+++ src/main/java/com/test/alliteration/AlliterationApplication.java
@@ -0,0 +1,15 @@
+package com.test.alliteration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlliterationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlliterationApplication.class, args);
+
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ src/test/java/com/test/alliteration/AlliterationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.alliteration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlliterationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
